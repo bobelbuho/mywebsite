01ae5d7e7a30e547e0cdaf70e79ffecb81a3d665
Bob <bob.vaz@hotmail.fr>
2024-02-14 18:43:54 +0100

    reponsive menu

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -7,7 +7,9 @@ export const Navbar = () => {
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">Portfolio</a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} src={getImageUrl("nav/menuIcon.png")} />
+            <img className={styles.menuBtn} 
+            src={getImageUrl("nav/menuIcon.png")} 
+            alt='menu-button'/>
             <ul className={styles.menuItems}>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
