829e024c34c5db35edf34c61a17b0482ba87ed5e
Bob <bob.vaz@hotmail.fr>
2024-02-14 18:49:40 +0100

    menujs

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -4,12 +4,19 @@ import { getImageUrl } from '../../utils';
 
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return <nav className={styles.navbar}>
         <a className={styles.title} href="/">Portfolio</a>
         <div className={styles.menu}>
-            <img className={styles.menuBtn} 
-            src={getImageUrl("nav/menuIcon.png")} 
-            alt='menu-button'/>
+            <img className={styles.menuBtn}
+                src={menuOpen
+                    ? getImageUrl("nav/closeIcon.png")
+                    : getImageUrl("nav/menuIcon.png")
+                }
+                alt='menu-button' 
+                onClick={() => setMenuOpen(!menuOpen)}
+                />
             <ul className={styles.menuItems}>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
