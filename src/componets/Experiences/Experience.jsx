0fdeca94acfa09ba08fd46c1b11e8e2db2d937d8
Bob <bob.vaz@hotmail.fr>
2024-02-21 19:05:26 +0100

    1h12

--- src/componets/Experiences/Experience.jsx
+++ src/componets/Experiences/Experience.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 
 import skills from '../../data/skills.json';
+import history from '../../data/history.json';
 import { getImageUrl } from '../../utils';
+
 
 export const Experience = () => {
   return <section id='experience'>
     <div>
         <div>
-            {skills.map((skill, id) => (
-                return <div key={id}>
-                    <div><img src={getImageUrl(skill.imageSrc)} alt={skill} /></div>
+            {skills.map((skill, id) => {
+                return (
+                <div key={id}>
+                    <div>
+                      <img src={getImageUrl(skill.imageSrc)} 
+                    alt={skill.title} />
+                    </div>
+                    <p>{skill.title}</p>
                 </div>
-            ))}
+                );
+                })}
         </div>
         <ul>
-
+          {
+            history.map((historyItem, id) => {
+              return (
+              <li key = {id}>
+                <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} />
+              </li>
+              )
+            })
+          }
         </ul>
     </div>
   </section>;
