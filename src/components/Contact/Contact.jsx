bcc837ab8721a3f62c44ca96c7c3126b896bcc89
Bob <bob.vaz@hotmail.fr>
2024-04-21 19:49:27 +0200

    update blank link

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,19 +15,19 @@ export const Contact = () => {
                 <img 
                 src={getImageUrl("contact/emailIcon.png")} 
                 alt="" />
-                <a href="mailto:bob.vaz@hotmail.fr">bob.vaz@hotmail.fr</a>
+                <a href="mailto:bob.vaz@hotmail.fr" target="_blank">bob.vaz@hotmail.fr</a>
             </li>
             <li className={styles.link}>
                 <img 
                 src={getImageUrl("contact/linkedinIcon.png")} 
                 alt="" />
-                <a href="https://www.linkedin.com/in/bob-vaz-5a981523a/">Linkedin</a>
+                <a href="https://www.linkedin.com/in/bob-vaz-5a981523a/" target="_blank">Linkedin</a>
             </li>
             <li className={styles.link}>
                 <img 
                 src={getImageUrl("contact/githubIcon.png")} 
                 alt="" />
-                <a href="https://github.com/bobelbuho">GitHub</a>
+                <a href="https://github.com/bobelbuho" target="_blank">GitHub</a>
             </li>
         </ul>
     </footer>
